331d55de8a47151d182ba0aa9222cba6b16cf982
Sai Rathan <52050053+RathanSreeramoju@users.noreply.github.com>
2020-01-09 16:32:09 -0500

    Second Activity

--- app/src/main/java/com/example/madpossystem/view/MainActivity.java
+++ app/src/main/java/com/example/madpossystem/view/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -17,7 +18,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onButtonClick(View v) {
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "My SEND Button is clicked!");
+    }
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+   /* public void onButtonClick(View v) {
         double v1 = 0.15;
         EditText e1 = (EditText)findViewById(R.id.editText);
         EditText e2 = (EditText)findViewById(R.id.editText2);
@@ -26,5 +31,5 @@ public class MainActivity extends AppCompatActivity {
         int num2 = Integer.parseInt(e2.getText().toString());
         int CALCULATE = (int) ((num1 * num2) * v1);
         t3.setText(Integer.toString(CALCULATE));
-    }
+    }*/
 }
